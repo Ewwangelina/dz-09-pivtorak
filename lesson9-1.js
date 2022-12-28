2e0adbeee6a586e6a49b2142b504b6d3c2524127
ewwangelina <kseniya1981pi@gmail.com>
2022-12-28 15:07:58 +0200

    dz-09-pivtorak

--- lesson9-1.js
+++ lesson9-1.js
@@ -1,18 +1,18 @@
-const inputKats1 = prompt('Enter two katets')
+const inputKats2 = prompt('Enter two katets')
 
-let gipoten1 = 0;
+let gipoTen2 = 0;
 
-if(inputKats1 !== null){
-    if(inputKats1.trim() !== ""){
-        const kats = inputKats1.split(" ")
+if(inputKats2 !== null){
+    if(inputKats2.trim() !== ""){
+        const kats = inputKats2.split(" ")
         console.log(kats[0], kats[1])
         console.log (isNaN (kats[0]))
         console.log (isNaN (kats[1]))
         if (isNaN (kats[0]) || isNaN (kats[1])) {
             alert(`Число не валідне ${kats[0], kats[1]}`)
         }else {
-            gipoten1 = Math.sqrt(Math.pow(kats[0], 2) + Math.pow(kats[1], 2))
-            alert(`Ваш результат ${gipoten1}`)
+          gipoTen2 = (kats[0] * kats[0]) + (kats[1] * kats[1])
+            alert(`Ваш результат ${gipoTen2}`)
         }
     }else {
         alert(`Строка пуста`)
@@ -20,4 +20,3 @@ if(inputKats1 !== null){
 }else {
     alert(`Ви скасували завдання`)
 }
- 
