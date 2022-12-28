d97906d66412c2b200c1228b196951c4010a1928
ewwangelina <kseniya1981pi@gmail.com>
2022-12-28 15:46:00 +0200

    correct skript

--- lesson9.js
+++ lesson9.js
@@ -5,12 +5,10 @@ let gipoten = 0;
 if(inputKats !== null){
     if(inputKats.trim() !== ""){
         const kats = inputKats.split(" ")
-        console.log(kats[0], kats[1])
-        console.log (isNaN (kats[0]))
-        console.log (isNaN (kats[1]))
-        if (isNaN (kats[0]) || isNaN (kats[1])) {
+        if ( Number(kats[0]) <=0 || Number((kats[1])<=0 )) {
             alert(`Число не валідне ${kats[0], kats[1]}`)
         }else {
+            console.log(kats[0], kats[1])
             gipoten = Math.sqrt(Math.pow(kats[0], 2) + Math.pow(kats[1], 2))
             alert(`Ваш результат ${gipoten}`)
         }
