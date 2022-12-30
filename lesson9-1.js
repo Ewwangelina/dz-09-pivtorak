5876b84e1389b850434615fe83bd2178de12df4f
ewwangelina <kseniya1981pi@gmail.com>
2022-12-30 13:33:31 +0200

    correction

--- lesson9-1.js
+++ lesson9-1.js
@@ -1,6 +1,6 @@
 const inputUser = prompt('Введить 2 катета через пробіл')
 
-if(inputUser?.trim() !== null) {
+if(inputUser.trim() !== null) {
     const arrCatet = inputUser.split(" ")
     if (Number(arrCatet[0]) > 0 && Number(arrCatet[1]) > 0 && arrCatet.length === 2) {
         let pow1 = arrCatet[0];
@@ -8,7 +8,7 @@ if(inputUser?.trim() !== null) {
         let pow2 = arrCatet[1];
         pow2**= 2;
         console.log(pow1 + pow2);
-        alert(`Ваш результат ${(pow1 + pow2)}`)
+        alert(`Ваш результат ${(pow1 + pow2)**(1/2)}`)
     }else {
         alert('Треба ввести два числа більше 0')}
 }else {
