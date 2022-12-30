ca5c80d8f2377778b07643fc98d91471246e7aba
ewwangelina <kseniya1981pi@gmail.com>
2022-12-30 13:27:34 +0200

    carrent

--- lesson9-1.js
+++ lesson9-1.js
@@ -1,12 +1,17 @@
-const inputUser = prompt("Введите 2 катета через пробел")
+const inputUser = prompt('Введить 2 катета через пробіл')
 
-if(inputUser?.trim() !== '' && inputUser !== null) {
+if(inputUser?.trim() !== null) {
     const arrCatet = inputUser.split(" ")
     if (Number(arrCatet[0]) > 0 && Number(arrCatet[1]) > 0 && arrCatet.length === 2) {
-        const firstCatet = arrCatet[0];
-        const secondCatet = arrCatet[1]
-        alert(`гіпотенуза = ${Math.sqrt((firstCatet**2)+ (secondCatet**2))}`)
+        let pow1 = arrCatet[0];
+        pow1**= 2;
+        let pow2 = arrCatet[1];
+        pow2**= 2;
+        console.log(pow1 + pow2);
+        alert(`Ваш результат ${(pow1 + pow2)}`)
     }else {
-        alert("треба ввести два числа більше 0")}
+        alert('Треба ввести два числа більше 0')}
 }else {
     alert('Нажаль вы нічого не ввели')}
+
+    
--- lesson9.js
+++ lesson9.js
@@ -1,18 +1,21 @@
-const inputKats = prompt('Enter two katets')
+const inputKats = prompt('Введить 2 катета через пробіл')
 
-let gipoten = 0;
+if(inputKats.trim !== null) {
+    const kats = inputKats.split(" ")
+    if ( Number(kats[0]) > 0 && Number((kats[1]) > 0  && kats.length === 2)) {
+        const firstKat = kats[0];
+        const secondKat = kats[1]
+        alert(`Ваш результат ${Math.sqrt(Math.pow(kats[0], 2) + Math.pow(kats[1], 2))}`) 
+    } else {
+        alert(`Треба ввести два числа більше 0`)
+    }
+} else {
+    alert('Нажаль вы нічого не ввели')}
 
-if(inputKats !== null){
-    if(inputKats.trim() !== ""){
-        const kats = inputKats.split(" ")
-        if ( Number(kats[0]) <=0 || Number((kats[1])<=0 )) {
-            alert(`Число не валідне ${kats[0], kats[1]}`)
-        }else {
-            console.log(kats[0], kats[1])
-            gipoten = Math.sqrt(Math.pow(kats[0], 2) + Math.pow(kats[1], 2))
-            alert(`Ваш результат ${gipoten}`)
-        }
-    }else {
-        alert('Нажаль вы нічого не ввели')}
-}else {
-    alert(`Ви скасували завдання`)}
+
+
+
+ 
+
+            
+
